e6f00e2ead88aaa055cad1009af79771396f6364
Umar Assa <u.assa.2007@gmail.com>
2026-09-25 18:01:17 +0500

    code clean up and add test route for checkout page

--- e-commerce-project/src/App.jsx
+++ e-commerce-project/src/App.jsx
@@ -6,7 +6,8 @@ import "./App.css";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />}></Route>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<div>Test check out page</div>} />
     </Routes>
   );
 }
