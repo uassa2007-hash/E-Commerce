b05246311b7cd44305341dd8b540de9cc2b1e154
Umar Assa <u.assa.2007@gmail.com>
2026-09-26 15:47:39 +0500

    move checkout page into react

--- e-commerce-project/src/App.jsx
+++ e-commerce-project/src/App.jsx
@@ -1,13 +1,14 @@
 // import { useState } from "react";
 import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/CheckoutPage";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="checkout" element={<div>Test check out page</div>} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   );
 }
--- e-commerce-project/src/pages/HomePage.jsx
+++ e-commerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import "./HomePage.css";
 export function HomePage() {
   return (
     <>
+      <title>Home Page</title>
       <div className="header">
         <div className="left-section">
           <a href="index.html" className="header-link">
