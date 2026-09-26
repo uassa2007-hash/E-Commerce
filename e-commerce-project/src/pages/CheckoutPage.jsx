7b2cfabab4768b85a31f68e0e06c2e2a1e597dec
Umar Assa <u.assa.2007@gmail.com>
2026-09-26 15:52:44 +0500

    update all the routes on pages

--- e-commerce-project/src/pages/CheckoutPage.jsx
+++ e-commerce-project/src/pages/CheckoutPage.jsx
@@ -8,7 +8,7 @@ export function CheckoutPage() {
       <div className="checkout-header">
         <div className="header-content">
           <div className="checkout-header-left-section">
-            <a href="index.html">
+            <a index>
               <img className="logo" src="images/logo.png" />
               <img className="mobile-logo" src="images/mobile-logo.png" />
             </a>
